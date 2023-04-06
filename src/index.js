1c1fd6d63542172f915743115f70a4a118ec8882
ne1nnn <fbn.alejandro@gmail.com>
2023-04-06 12:52:40 -0400

    changes in navbar
    new navbar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Product from "./components/product";
+import Product from "./components/products/product";
 import Navbar from "./components/navbar/navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
